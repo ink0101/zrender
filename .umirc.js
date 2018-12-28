1e7aa91546fb373646f04bf45b7c14b0ece1e118
ink0101 <cftt2016@gmail.com>
2018-12-28 10:54:25 +0800

    [add] 环境搭建

---
+++ .umirc.js
@@ -0,0 +1,40 @@
+let path = require('path');
+export default {
+  plugins: [
+    ['umi-plugin-react', {
+      dva: {
+        immer: true,
+      },
+      antd: true,
+      singular: true,
+      title: {
+        defaultTitle: '指标库'
+      }
+    }]
+  ],
+  lessLoaderOptions: {
+    javascriptEnabled: true
+  },
+  alias: {
+    "assets": path.resolve(__dirname, 'src/assets/')
+  },
+  disableDynamicImport: false,
+  theme: {
+    // body 背景色
+    '@body-background': '#f3f3f3',
+    // 主色
+    "@primary-color": '#3495fe',
+    // 链接色
+    '@link-color': '#3495fe',
+    // 主体文字大小
+    "@font-size-base": '12px',
+    // 常规文字
+    '@text-color': '#000000',
+    // 次级文字
+    '@text-color-secondary': '#666666',
+    // 失效文字
+    '@disabled-color': '#c0c0c0',
+    // 表格表头背景色
+    '@table-header-bg': '#dfebf5',
+  }
+}
